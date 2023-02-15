04a37946188b7e43db4f3f0deb7b9b2c882dce34
Leandro Hermes Neto <leandroneto@movida.com.br>
2023-02-15 11:00:02 -0300

    Support int backed enums in QueryBuilder::whereIntegerInRaw

--- src/Illuminate/Database/Query/Builder.php
+++ src/Illuminate/Database/Query/Builder.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Traits\ForwardsCalls;
 use Illuminate\Support\Traits\Macroable;
 use InvalidArgumentException;
 use LogicException;
+use ReflectionEnum;
 use RuntimeException;
 
 class Builder implements BuilderContract
@@ -1147,7 +1148,11 @@ class Builder implements BuilderContract
         $values = Arr::flatten($values);
 
         foreach ($values as &$value) {
-            $value = (int) $value;
+            if ($value instanceof BackedEnum && (string) (new ReflectionEnum($value))->getBackingType() === 'int') {
+                $value = $value->value;
+            } else {
+                $value = (int) $value;
+            }
         }
 
         $this->wheres[] = compact('type', 'column', 'values', 'boolean');
